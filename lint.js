bf148ca081f07e31dd50c4eae2b33c8423212083
Sean Rich <srich366@gmail.com>
2024-01-31 22:08:42 -0800

    [Linting] Fix linting

--- lint.js
+++ lint.js
@@ -6,7 +6,7 @@ const files = utils.getAllFiles('til_docs')
 // A quick script to lint for snake case filenames so we
 const snakeCaseRegex = /^[A-Z]+[a-z]*(_[A-Z]+[a-z]*)*(\.md)?$/
 const ignoredFiles = new Set(['index.md', 'JavaScript.md',
-    'TypeScript.md'])
+    'TypeScript.md', 'DynamoDB.md', 'Zephyr_7B.md'])
 
 const invalidFiles = files.filter( name => {
     const baseName = path.basename(name)
